e3c581b7326e7869ce4259603969a40b48c503f8
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-07-08 10:10:38 -0800

    Removed image signing from GH action

---
+++ test/test-handler.py
@@ -0,0 +1,8 @@
+from unittest.mock import MagicMock
+import unittest
+
+class TestBotHandler(unittest.TestCase):
+    def test_simple(self):
+        self.assertTrue("one", "one")
+
+unittest.main()
